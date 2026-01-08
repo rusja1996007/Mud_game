26d226f953e636a44095ef02459f00afee41bead
Ruslan Big <rusja1996007@mail.ru>
2026-01-08 15:02:21 +0300

    perviy commit

---
+++ feature1/feature1.go
@@ -0,0 +1,7 @@
+package feature1
+
+import "fmt"
+
+func Feauture1() {
+	fmt.Println("This is feature 1 !!!")
+}
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"study/feature1"
+)
+
+func main() {
+	fmt.Println("Hello gei!")
+	feature1.Feauture1()
+}
