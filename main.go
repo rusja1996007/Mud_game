e5c497e304780d9ed0211b677abd9e691519da99
Ruslan Big <rusja1996007@mail.ru>
2026-01-13 13:32:01 +0300

    end

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"study/feature1"
+	"study/feature2"
+)
+
+// /....................///
+func main() {
+	fmt.Println("Hello gei!")
+	feature1.Feauture1()
+	feature2.Feature2()
+}
