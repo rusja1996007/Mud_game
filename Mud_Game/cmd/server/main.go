24002056c4cbda41b758160177bead9cf5bc1cdd
Ruslan <rusja1996007@mail.ru>
2026-02-13 22:14:03 +0300

    logger+config

--- Mud_Game/cmd/server/main.go
+++ Mud_Game/cmd/server/main.go
@@ -1,1 +1,17 @@
-package mudgame
+package main
+
+import (
+	"Mud_game/Mud_Game/internal/pkg/config"
+	"Mud_game/Mud_Game/internal/pkg/logger"
+	"fmt"
+)
+
+func main() {
+	log := logger.NewSimpleLogger("MAIN")
+	cfg := config.DefaultConfig()
+
+	log.Info("Сервер запускается...")
+	log.Info(fmt.Sprintf("Название: %s", cfg.NameServer))
+	log.Info(fmt.Sprintf("Порт: %d", cfg.Port))
+
+}
